f8718dee6edc9c92794b27968926e182b54f225b
Jeffrey Chen <jeffrey@hiis.org>
2015-10-14 09:30:56 +1100

    Change namespace

---
+++ Utility/org.hiis.utility.Luhn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace org.hiis.Utility {
+	public static class Luhn {
+		public static string GenerateCheckSum(string id) {
+			var sum = 0;
+			var alt = true;
+			var digits = id.ToCharArray();
+			for (int i = digits.Length - 1; i >= 0; i--) {
+				var curDigit = (digits[i] - 48);
+				if (alt) {
+					curDigit *= 2;
+					if (curDigit > 9)
+						curDigit -= 9;
+				}
+				sum += curDigit;
+				alt = !alt;
+			}
+			if ((sum % 10) == 0) {
+				return "0";
+			}
+			return (10 - (sum % 10)).ToString();
+		}
+	}
+}
--- org.hiis.security.cs
+++ org.hiis.security.cs
@@ -2,29 +2,5 @@
 
 namespace org.hiis {
 	public class security {
-		/// <summary>
-		/// Generate the luhn 
-		/// </summary>
-		/// <param name="id"></param>
-		/// <returns></returns>
-		public static string luhn(string id) {
-			var sum = 0;
-			var alt = true;
-			var digits = id.ToCharArray();
-			for (int i = digits.Length - 1; i >= 0; i--) {
-				var curDigit = (digits[i] - 48);
-				if (alt) {
-					curDigit *= 2;
-					if (curDigit > 9)
-						curDigit -= 9;
-				}
-				sum += curDigit;
-				alt = !alt;
-			}
-			if ((sum % 10) == 0) {
-				return "0";
-			}
-			return (10 - (sum % 10)).ToString();
-		}
 	}
 }
