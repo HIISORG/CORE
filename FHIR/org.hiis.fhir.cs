7506e512f339f41fa4bd1a8192fa0f0f7dc13ef3
Jeffrey Chen <jeffrey@hiis.org>
2015-12-14 10:27:28 +1100

    Initial FHIR class

---
+++ FHIR/org.hiis.fhir.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace org.hiis.FHIR {
+	class Person {
+
+	}
+
+	class Condition {
+
+	}
+
+	class Procedure {
+
+	}
+
+	class DiagnosticReport {
+
+	}
+}
